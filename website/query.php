a8c2bbbbccdd8b44630814152e0ba330b6b789c7
Peter Lenahan <Peter.Lenahan84@myhunter.cuny.edu>
2015-06-25 03:43:40 -0400

    Added support for user creditials to be passed to query wrapper

--- website/query.php
+++ website/query.php
@@ -7,16 +7,27 @@ if (isset($_GET["db"])) {
 else {
         $paramDb = "weather";
 }
+if (isset($_GET["u"])) {
+	$paramUser = rawurlencode($_GET["u"]);
+} else {
+        $paramUser = "weather_user";
+}
+if (isset($_GET["p"])) {
+	$paramPassword = rawurlencode($_GET["p"]);
+} else {
+        $paramPassword = "weather_password";
+}
 if (isset($_GET["query"])) {
 	$paramQuery = rawurlencode($_GET["query"]);
-}
-else {
+} else if (isset($_GET["q"])) {
+	$paramQuery = rawurlencode($_GET["q"]);
+} else {
         $paramQuery = rawurlencode("LIST SERIES");
 }
 
 //This is cluster admin permission, only use if you need cluster admin functionality
 //$getURL = "http://weatherspot.us:8086/db/" . $paramDb . "/series?u=root&p=root&q=" . $paramQuery;		
-$getURL = "http://weatherspot.us:8086/db/" . $paramDb . "/series?u=weather_user&p=weather_password&q=" . $paramQuery; 
+$getURL = "http://weatherspot.us:8086/db/" . $paramDb . "/series?u=" . $paramUser . "&p=" . $paramPassword . "&q=" . $paramQuery; 
 
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, $getURL); 
