fb2e34764c7ef6f94ec19977f588fbfc98ca86c2
Peter Lenahan <Peter.Lenahan84@myhunter.cuny.edu>
2015-06-25 15:09:05 -0400

    Code cleanup

--- website/query.php
+++ website/query.php
@@ -1,36 +1,28 @@
 
 <?php
 
-if (isset($_GET["db"])) {
-        $paramDb = rawurlencode($_GET["db"]);
-}
-else {
-        $paramDb = "weather";
-}
-if (isset($_GET["u"])) {
-	$paramUser = rawurlencode($_GET["u"]);
-} else {
-        $paramUser = "weather_user";
-}
-if (isset($_GET["p"])) {
-	$paramPassword = rawurlencode($_GET["p"]);
-} else {
-        $paramPassword = "weather_password";
-}
-if (isset($_GET["query"])) {
-	$paramQuery = rawurlencode($_GET["query"]);
-} else if (isset($_GET["q"])) {
-	$paramQuery = rawurlencode($_GET["q"]);
-} else {
-        $paramQuery = rawurlencode("LIST SERIES");
-}
+$paramDb = test_GETSetOrDefault("db", "weather");
+$paramUser = test_GETSetOrDefault("u", "weather_user");
+$paramPassword = test_GETSetOrDefault("p", "weather_password");
+$paramQuery = test_GETSetOrDefault("query", test_GETSetOrDefault("q", "LIST+SERIES"));	//Chain them together to test "query" or "q"
 
 //This is cluster admin permission, only use if you need cluster admin functionality
 //$getURL = "http://weatherspot.us:8086/db/" . $paramDb . "/series?u=root&p=root&q=" . $paramQuery;		
 $getURL = "http://weatherspot.us:8086/db/" . $paramDb . "/series?u=" . $paramUser . "&p=" . $paramPassword . "&q=" . $paramQuery; 
+doCurl($getURL);
 
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $getURL); 
-$result = curl_exec($curl);
-curl_close($curl);
+function test_GETSetOrDefault($test, $default) {
+	// Test if a parameter is set in the URL, otherwise return default value
+	if (isset($_GET[$test])) {
+		return rawurlencode($_GET[$test]);
+	} else {
+		return $default; 
+	}
+}
+function doCurl($url) {
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $url); 
+	$result = curl_exec($curl);
+	curl_close($curl);
+}
 ?>
